a2798c3aefde8c97ba21f0c4354aece583270818
martingathu <martin5gathu@gmail.com>
2020-03-01 17:56:59 +0300

    creat getSize function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+ function getSize (size) {
+     if (size === 0) {
+         return "Pizza Pie"
+    }else if (size === 1) {
+         return "Regular"
+     }else if (size === 2) {
+         return "Medium"
+     }else if (size === 3) {
+      return "large"
+    }
+  };
+ 
+ 
