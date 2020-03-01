c6de53edc0c75c9c7ca8f48a74e75d5bea993ea8
martingathu <martin5gathu@gmail.com>
2020-03-01 18:01:37 +0300

    create a contact form validation function

--- js/script.js
+++ js/script.js
@@ -42,6 +42,22 @@
       });
 
 
+    //contacts user information validation
+  $("#submit").click(function () {
+      event.preventDefault()
+      var name = $("#name").val();
+      var email = $("#email").val();
+      var message = $("#message").val();
+
+      if (name == '' || email == '' || message == '') {
+        $("#post").html("Please fill all fields");
+        $(".post").css('background-color', 'red')
+      } else {
+        $("#post").html(name + " we have received your message. Thank you for reaching out to us")
+        $(".post").css('background-color', 'green')
+        $("form")[0].reset();
+      }
+    });
     //show the delivery form
     $("#delivery").click(function(){
       $(".delivery").show()
